98e0c8deda11dbb1883eeaa359c0767ed9d575b6
Frank Luongo <frank@frankluongo.com>
2022-11-01 20:19:57 -0400

    what the font

--- src/app/fonts/fonts.js
+++ src/app/fonts/fonts.js
@@ -2,40 +2,41 @@ import localFont from "@next/font/local";
 
 export const silkaBlack = localFont({
   src: "./silka/silka-black.woff2",
-  weight: "900",
-  style: "normal",
-  variable: "--silka-black",
+  // weight: "900",
+  // style: "normal",
+  // variable: "--silka-black",
 });
 export const silkaBold = localFont({
   src: "./silka/silka-bold.woff2",
-  weight: "600",
-  style: "normal",
-  variable: "--silka-bold",
+  // weight: "600",
+  // style: "normal",
+  // variable: "--silka-bold",
 });
 export const silkaBoldItalic = localFont({
   src: "./silka/silka-bolditalic.woff2",
-  weight: "600",
-  style: "italic",
-  variable: "--silka-bold-italic",
+  // weight: "600",
+  // style: "italic",
+  // variable: "--silka-bold-italic",
 });
 export const silkaRegular = localFont({
   src: "./silka/silka-regular.woff2",
-  weight: "400",
-  style: "normal",
+  // weight: "400",
+  // style: "normal",
   variable: "--silka-regular",
+  // fallback: "Silka Regular, sans-serif",
 });
 export const silkaRegularItalic = localFont({
   src: "./silka/silka-regularitalic.woff2",
-  weight: "400",
-  style: "italic",
-  variable: "--silka-regular-italic",
+  // weight: "400",
+  // style: "italic",
+  // variable: "--silka-regular-italic",
 });
 
 export const silkaMonoRegular = localFont({
   src: "./silka-mono/silkamono-regular.woff2",
-  weight: "400",
-  style: "normal",
-  variable: "--silka-mono-regular",
+  // weight: "400",
+  // style: "normal",
+  // variable: "--silka-mono-regular",
 });
 
 export const fontVars = [
